6251bd14509bb2cd48ba7a458d62cc4956b3826c
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-01-29 18:02:46 +0300

    fix

--- player.gd
+++ player.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var move_range = 75
-var start_transform = 0
 
 func _ready():
-	start_transform = position.y
 	$AnimatedSprite2D.play()
 	
 func _physics_process(delta):
